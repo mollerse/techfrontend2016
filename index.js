24f8315a3467090ffba949ae297dd71a98190749
Stian Veum Møllersen <stian.moellersen@gmail.com>
2015-11-10 21:53:18 +0100

    Remove scrollTo code

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-var scrollTo = require('scroll-to');
 var vivus = require('vivus');
-
-function handleDown(e) {
-  e.preventDefault();
-
-  scrollTo(0, window.innerHeight, {
-    ease: 'outSine',
-    duration: 500
-  });
-}
-
-document.querySelector('.down a').addEventListener('click', handleDown);
-document.querySelector('.down a').addEventListener('touchdown', handleDown);
 
 new vivus('background', {duration: 100, type: 'oneByOne'});
