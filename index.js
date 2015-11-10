c621dbc23f9f813cc12f4aaeb18b2caeca1a0b00
Erik Wendel <erik@wendel.no>
2015-11-10 22:16:05 +0100

    tweaked animation timings

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ new vivus('background', {duration: 50, type: 'oneByOne', start: 'autostart'});
 var sections = document.querySelectorAll('.two-column section');
 setTimeout(function() {
   sections[0].classList.add('fade-in');
-}, 1000);
+}, 300);
 
 setTimeout(function() {
   sections[1].classList.add('fade-in');
-}, 1500);
+}, 600);
